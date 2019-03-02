ac463e5201aec5b2914d7d1f24eeb7a4198dc130
Maya Lavi <nladino215@gmail.com>
2019-03-02 17:26:15 +0000

    worked on problem set

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,1 +1,23 @@
 # file name: problemSetDay4.py
+num = 2
+guess = int(input('pick a number between 1-10'))
+while guess != 2:
+    guess = int(input('try again'))
+print('good job')
+
+strength = 5
+print(strength)
+while strength <= 10:
+    strength += 2
+    print(strength)
+print('you are strong enough to advance')
+
+n = 3
+m = 2
+print(n ** m)
+
+n = 3
+m = 2
+while n  != 3** m:
+    n *= n
+    print(n)
